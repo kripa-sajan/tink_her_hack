0a5e48080d4de78d6b75196c3c039fa0dae30545
= <=>
2024-03-09 21:33:27 +0530

    final

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const passwordInput = document.querySelector(".pass-field input");
 const eyeIcon = document.querySelector(".pass-field i");
 const requirementList = document.querySelectorAll(".requirement-list li");
+const suggestionMessage = document.querySelector(".suggestion-message");
 
+// An array of password requirements with corresponding 
+// regular expressions and index of the requirement list item
 const requirements = [
-    { regex: /.{8,}/, index: 0 }, 
-    { regex: /[0-9]/, index: 1 }, 
-    { regex: /[a-z]/, index: 2 }, 
-    { regex: /[^A-Za-z0-9]/, index: 3 }, 
-    { regex: /[A-Z]/, index: 4 }, 
+    { regex: /.{8,}/, index: 0 }, // Minimum of 8 characters
+    { regex: /[0-9]/, index: 1 }, // At least one number
+    { regex: /[a-z]/, index: 2 }, // At least one lowercase letter
+    { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
+    { regex: /[A-Z]/, index: 4 }, // At least one uppercase letter
 ]
+
 passwordInput.addEventListener("keyup", (e) => {
     requirements.forEach(item => {
-        
+        // Check if the password matches the requirement regex
         const isValid = item.regex.test(e.target.value);
         const requirementItem = requirementList[item.index];
-      
+        // Updating class and icon of requirement item if requirement matched or not
         if (isValid) {
             requirementItem.classList.add("valid");
             requirementItem.firstElementChild.className = "fa-solid fa-check";
@@ -23,10 +27,26 @@ passwordInput.addEventListener("keyup", (e) => {
             requirementItem.firstElementChild.className = "fa-solid fa-circle";
         }
     });
-});
-eyeIcon.addEventListener("click", () => {
     
+    // Check password strength and update suggestion message
+    const strength = getPasswordStrength(e.target.value);
+    suggestionMessage.textContent = `Password Strength: ${strength}`;
+});
+
+eyeIcon.addEventListener("click", () => {
+    // Toggle the password input type between "password" and "text"
     passwordInput.type = passwordInput.type === "password" ? "text" : "password";
-   
+    // Update the eye icon class based on the password input type
     eyeIcon.className = `fa-solid fa-eye${passwordInput.type === "password" ? "" : "-slash"}`;
 });
+
+// Function to calculate password strength
+function getPasswordStrength(password) {
+    let strength = 0;
+    requirements.forEach(item => {
+        if (item.regex.test(password)) {
+            strength++;
+        }
+    });
+    return strength;
+}
